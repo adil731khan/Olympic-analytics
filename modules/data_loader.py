0320b9b0ee88e5e1217eb69b41b2954f83097a8d
Adil Khan <adil731khan@gmail.com>
2026-07-26 17:44:39 +0530

    Update data_loader.py

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 @st.cache_data
 def load_data():
-    athlete = pd.read_csv("data/athlete_events.csv")
-    regions = pd.read_csv("data/noc_regions.csv")
+    athlete = pd.read_csv("data/athlete_events.csv.zip")
+    regions = pd.read_csv("data/noc_regions.csv.zip")
     athlete = athlete.drop_duplicates()
     athlete = athlete.merge(regions,on="NOC",how="left")
     athlete["region"] = athlete["region"].fillna("Unknown")
